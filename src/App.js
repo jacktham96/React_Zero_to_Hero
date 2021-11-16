3bfcf80cf90b2dea7dc6a67b5562f3a950aa6907
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2021-11-16 23:46:39 +0800

    小 Chat room 完成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import Header from "./components/Header";
 import BottomNav from "./components/BottomNav";
 import Main from "./pages/Main";
+import {useState} from "react";
+
+function ChatBox({image,message , right}) {
+
+  const element = [
+    <div>
+      <img className="rounded-full w-48" src={image} />
+    </div> ,   
+    <p className="bg-gray-200 p-4 rounded-lg ">{message}</p> 
+  ]
+
+  return (
+    <div className="flex space-x-4">
+      {right ? element.reverse() : element}
+    </div>
+  )
+}
+
+
+function ChatRoom({message}) {
+
+  return (
+    <div className="px-4 py-4 space-y-5">
+      {
+        message.map((msg, index) =>(
+          <ChatBox image={msg.image} message={msg.message} right={index % 2} />
+        ))
+      }
+    </div>
+  )
+}
+
 
 function App() {
+  const [message, setMessage] = useState([]);
+
+  console.log(message.length)
+
   return (
     <div>
       <Header title="Chat" canBack/>
-      <Main />
-      <BottomNav />
+      <ChatRoom message={message}/>
+      <BottomNav onPlusClick={() => {
+        setMessage([...message, {
+          image : 'https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/M01/012/50/M010125046.jpg&v=606e78c0&w=280&h=280',
+          message : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        }])
+      }}
+      />
     </div>
   );
 }
--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,22 +5,26 @@ import { ReactComponent as PERSON_ICON } from '../assets/icons/person.svg'
 import { ReactComponent as PLUS_ICON } from '../assets/icons/plus.svg'
 
 
-export default function BottomNav() {
+export default function BottomNav({onPlusClick}) {
     return(
-      <footer className="fixed bottom-0 w-full">
+      <footer className="fixed bottom-0 w-full bg-white">
         <nav className="w-full flex justify-between">
           <button className="p-4">
             <HOME_ICON className="w-4"/>
           </button>
+
           <button className="p-4">
             <SEARCH_ICON className="w-4"/>
           </button>
-          <button className="px-6 py-4">
+
+          <button className="px-6 py-4" onClick = {onPlusClick}>
             <PLUS_ICON className="w-4"/>
           </button>
+
           <button className="p-4">
             <CHAT_ICON className="w-4"/>
           </button>
+
           <button className="p-4">
             <PERSON_ICON className="w-4"/>
           </button>
